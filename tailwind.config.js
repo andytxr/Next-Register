0535d74573b4b86f92124b280630e80d12d71b43
andytxr <andreluiiz.gst@gmail.com>
2022-05-19 17:25:28 -0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsw,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx'
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
